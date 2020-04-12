4db843e100b61e996bb096e4de20f53e96abd727
Jojo Duan <jiujiuduan@gmail.com>
2020-04-11 23:30:23 -0400

    basket

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchStream , addToBasket} from '../../actions'
+//import { fetchStream , addToBasket} from '../../actions'
+import { addToBasket} from '../../actions'
 
 class StreamShow extends React.Component {
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        //this.props.fetchStream(this.props.match.params.id)
     }
     addTo(){
         console.log("add this to basket")
-        this.props.addToBasket(this.props.stream)
+        this.props.addToBasket(this.props.book)
 
     }
     render() {
@@ -28,7 +29,6 @@ class StreamShow extends React.Component {
                     <h5>{"royalty_rate: " + book.royalty_rate}</h5>
                     <h5>{"ISBN: " + book.isbn}</h5>
                     <h5>{"num of pages: " + book.num_page}</h5>
-                    <h5>{"ISBN: " + book.isbn}</h5>
                     <h5>{"inventory: " + book.inventory}</h5>
                     <h5>{"genre_name: " + book.genre_name}</h5>
                     <button onClick={() => { this.addTo()}} className="ui button">Add to Basket</button>
@@ -51,4 +51,5 @@ const findBook = (books, id) =>{
     }
     return null
 }
-export default connect(mapStateToProps, { fetchStream , addToBasket})(StreamShow)
+//export default connect(mapStateToProps, { fetchStream , addToBasket})(StreamShow)
+export default connect(mapStateToProps, { addToBasket })(StreamShow)

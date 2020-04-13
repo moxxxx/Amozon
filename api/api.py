e0188d544b1fa671ad8db8c217d9210e050c1e97
Kirk-Zhen <48104232+Kirk-Zhen@users.noreply.github.com>
2020-04-13 19:52:24 -0400

    Add files via upload

--- api/api.py
+++ api/api.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from flask_cors import CORS
-from flask import jsonify
 import psycopg2
 
 
